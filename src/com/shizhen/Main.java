af56f7bb0c71c93e54d82170a1cec52c1ec146ee
shizhen5452 <shizhen5452@gmail.com>
2018-10-25 09:03:35 +0800

    add Delete Operation for Two Strings

--- src/com/shizhen/Main.java
+++ src/com/shizhen/Main.java
@@ -8,6 +8,7 @@ public class Main {
         //TreeNode treeNode = ConstructBinaryTree.buildTree(new int[]{9, 3, 15, 20, 7}, new int[]{9, 15, 7, 20, 3});
         //System.out.println(GlobalAndLocalInversions.isIdealPermutation(new int[]{1,2,0}));
         //System.out.println(CoinChange.coinChange(new int[]{1,2,5},11));
+        //System.out.println(DeleteOperationForTwoStrings.minDistance("snerea","ieatuupo"));
     }
 
 }
